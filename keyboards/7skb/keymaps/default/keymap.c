328a35b14876f0c016470b9d29de700ec5a36f50
junju <junju@KURO1.localdomain>
2020-07-29 02:44:28 +0900

    try layer led

--- keyboards/7skb/keymaps/default/keymap.c
+++ keyboards/7skb/keymaps/default/keymap.c
@@ -22,7 +22,11 @@ enum layer_number {
   RGB_RST = SAFE_RANGE
 };*/
 
-enum custom_keycodes { JP = SAFE_RANGE, US, UBUNTU, MAC, SHIFT, LOWER, RAISE, ADJUST, CSTM_0, CSTM_1, CSTM_2, CSTM_3, CSTM_4, CSTM_5, CSTM_6, CSTM_7, CSTM_8, CSTM_9, CIRC, AT, LBRC, RBRC, BSLS, AMPR, QUOT, LPRN, RPRN, EQL, TILD, PIPE, GRV, LCBR, PLUS, ASTR, RCBR, UNDS, MINS, SCLN, COMM, DOT, SLSH, EXLM, HASH, DLR, PERC, DEL, DESKDN, DESKUP, RGB_RST = SAFE_RANGE };
+enum custom_keycodes { JP = SAFE_RANGE,
+ US, 
+ UBUNTU, 
+ MAC, 
+ SHIFT, LOWER, RAISE, ADJUST, CSTM_0, CSTM_1, CSTM_2, CSTM_3, CSTM_4, CSTM_5, CSTM_6, CSTM_7, CSTM_8, CSTM_9, CIRC, AT, LBRC, RBRC, BSLS, AMPR, QUOT, LPRN, RPRN, EQL, TILD, PIPE, GRV, LCBR, PLUS, ASTR, RCBR, UNDS, MINS, SCLN, COMM, DOT, SLSH, EXLM, HASH, DLR, PERC, DEL, DESKDN, DESKUP, RGB_RST};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_QWERTY] = LAYOUT(
                                                                   //,-----------------------------------------------------|   |--------------------------------------------------------------------------------.
@@ -34,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_QWERTY] = LAYOUT
                                                                   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
                                                                   SHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, SHIFT, MO(_FN),
                                                                   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-                                                                  KC_LCTRL, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_UP, KC_DOWN
+                                                                  KC_LCTRL, KC_LALT, KC_SPC, MO(_FN), KC_SPC, KC_RGUI, KC_UP, KC_DOWN
                                                                   //`---------------------------------------------|   |--------------------------------------------'
                                                                   ),
 
@@ -42,13 +46,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_QWERTY] = LAYOUT
                                                                   //,-----------------------------------------------------|   |--------------------------------------------------------------------------------.
                                                                   TG(_ADJUST), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,
                                                                   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
-                                                                  _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUSE, KC_UP, _______, KC_BSPC,
+                                                                  _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, _______, _______, KC_PSCR, KC_SLCK, KC_PAUSE, KC_UP, _______, KC_BSPC,
                                                                   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
                                                                   _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT, _______,
                                                                   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
                                                                   _______, _______, _______, _______, _______, _______, _______, _______, KC_END, KC_PGDN, KC_DOWN, _______, _______,
                                                                   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-                                                                  _______, _______, _______, _______, _______, _______, KC_STOP, _______
+                                                                  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_RIGHT
                                                                   //`---------------------------------------------|   |--------------------------------------------'
                                                                   ),
 
@@ -65,29 +69,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_QWERTY] = LAYOUT
                                                                                  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_STOP, XXXXXXX
                                                                                  //`---------------------------------------------|   |--------------------------------------------'
                                                                                  )};
-#define JP_LAYOUT true
-#define US_LAYOUT false
-bool LAYOUT_STATUS = JP_LAYOUT;
-bool SHIFT_PRESSED = false;
 
 // A description for expressing the layer position in LED mode.
 layer_state_t layer_state_set_user(layer_state_t state) {
 #ifdef RGBLIGHT_ENABLE
     switch (get_highest_layer(state)) {
         case _FN:
-            rgblight_sethsv_at(HSV_BLUE, 0);
+            //rgblight_sethsv_at(HSV_BLUE, 0);
+            rgblight_disable();
             break;
         case _ADJUST:
             rgblight_sethsv_at(HSV_PURPLE, 0);
             break;
         default:  //  for any other layers, or the default layer
+            rgblight_enable();
             rgblight_sethsv_at(0, 0, 0, 0);
             break;
     }
-    rgblight_set_effect_range(1, 4);
+    rgblight_set_effect_range(1, 1);
 #endif
     return state;
 }
+
+#define JP_LAYOUT true
+#define US_LAYOUT false
+bool LAYOUT_STATUS = JP_LAYOUT;
+bool SHIFT_PRESSED = false;
 
 #define SEND_STRING_RESTORE(STR) (SHIFT_PRESSED ? SEND_STRING(STR SS_DOWN(X_LSHIFT)) : SEND_STRING(STR SS_UP(X_LSHIFT)))
 
@@ -104,12 +111,47 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         (LAYOUT_STATUS == JP_LAYOUT ? JP : US); \
         return false;
 
-int  RGB_current_mode;
+int RGB_current_mode;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    //bool result = false;
+    // bool result = false;
     switch (keycode) {
-/*#ifdef RGBLIGHT_ENABLE
+        CASE_US(CSTM_0, KEY(0), SHIFT_DU(KEY_SHIFT(9), KEY(0)));
+        CASE_US(CSTM_1, KEY(1), KEY(1));
+        CASE_US(CSTM_2, KEY(2), SHIFT_DU(KEY_UPSHIFT(LBRACKET), KEY(2)));
+        CASE_US(CSTM_3, KEY(3), KEY(3));
+        CASE_US(CSTM_4, KEY(4), KEY(4));
+        CASE_US(CSTM_5, KEY(5), KEY(5));
+        CASE_US(CSTM_6, KEY(6), SHIFT_DU(KEY_UPSHIFT(EQUAL), KEY(6)));
+        CASE_US(CSTM_7, KEY(7), SHIFT_DU(KEY_SHIFT(6), KEY(7)));
+        CASE_US(CSTM_8, KEY(8), SHIFT_DU(KEY_SHIFT(QUOTE), KEY(8)));
+        CASE_US(CSTM_9, KEY(9), SHIFT_DU(KEY_SHIFT(8), KEY(9)));
+        CASE_US(DEL, KEY(DELETE), KEY_UPSHIFT(BSPACE));
+        CASE_US(TILD, KEY_SHIFT(GRAVE), KEY_SHIFT(EQUAL));
+        CASE_US(EXLM, KEY_SHIFT(1), KEY_SHIFT(1));
+        CASE_US(AT, KEY_SHIFT(2), KEY(LBRACKET));
+        CASE_US(HASH, KEY_SHIFT(3), KEY_SHIFT(3));
+        CASE_US(DLR, KEY_SHIFT(4), KEY_SHIFT(4));
+        CASE_US(PERC, KEY_SHIFT(5), KEY_SHIFT(5));
+        CASE_US(CIRC, KEY_SHIFT(6), KEY(EQUAL));
+        CASE_US(AMPR, KEY_SHIFT(7), KEY_SHIFT(6));
+        CASE_US(ASTR, KEY_SHIFT(8), KEY_SHIFT(QUOTE));
+        CASE_US(LPRN, KEY_SHIFT(9), KEY_SHIFT(8));
+        CASE_US(RPRN, KEY_SHIFT(0), KEY_SHIFT(9));
+        CASE_US(KC_LBRC, KEY(LBRACKET), SHIFT_DU(KEY_SHIFT(RBRACKET), KEY(RBRACKET)));
+        CASE_US(KC_RBRC, KEY(RBRACKET), SHIFT_DU(KEY_SHIFT(NONUS_HASH), KEY(NONUS_HASH)));
+        CASE_US(LCBR, KEY_SHIFT(LBRACKET), KEY_SHIFT(RBRACKET));
+        CASE_US(RCBR, KEY_SHIFT(RBRACKET), KEY_SHIFT(NONUS_HASH));
+        CASE_US(GRV, KEY(GRAVE), SHIFT_DU(KEY_SHIFT(EQUAL), KEY_SHIFT(LBRACKET)));
+        CASE_US(KC_BSLS, KEY(BSLASH), SHIFT_DU(KEY_SHIFT(INT3), KEY(INT3)));
+        // CASE_US(PIPE, KEY_SHIFT(BSLASH), KEY_SHIFT(INT3));
+        CASE_US(KC_MINS, KEY(MINUS), SHIFT_DU(KEY_SHIFT(INT1), KEY(MINUS)));
+        // CASE_US(UNDS, KEY_SHIFT(MINUS), KEY_SHIFT(INT1));
+        CASE_US(KC_EQL, KEY(EQUAL), SHIFT_DU(KEY_SHIFT(SCOLON), KEY_SHIFT(MINUS)));
+        // CASE_US(KC_PLUS, KEY_SHIFT(EQUAL), KEY_SHIFT(SCOLON));
+        CASE_US(KC_SCLN, KEY(SCOLON), SHIFT_DU(KEY_UPSHIFT(QUOTE), KEY(SCOLON)));
+        CASE_US(KC_QUOT, KEY(QUOTE), SHIFT_DU(KEY_SHIFT(2), KEY_SHIFT(7)));
+#ifdef RGBLIGHT_ENABLE
         case RGB_MOD:
             if (record->event.pressed) {
                 rgblight_mode(RGB_current_mode);
@@ -124,42 +166,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 RGB_current_mode = rgblight_config.mode;
             }
             break;
-#endif*/
-            CASE_US(CSTM_0, KEY(0), SHIFT_DU(KEY_SHIFT(9), KEY(0)));
-            CASE_US(CSTM_1, KEY(1), KEY(1));
-            CASE_US(CSTM_2, KEY(2), SHIFT_DU(KEY_UPSHIFT(LBRACKET), KEY(2)));
-            CASE_US(CSTM_3, KEY(3), KEY(3));
-            CASE_US(CSTM_4, KEY(4), KEY(4));
-            CASE_US(CSTM_5, KEY(5), KEY(5));
-            CASE_US(CSTM_6, KEY(6), SHIFT_DU(KEY_UPSHIFT(EQUAL), KEY(6)));
-            CASE_US(CSTM_7, KEY(7), SHIFT_DU(KEY_SHIFT(6), KEY(7)));
-            CASE_US(CSTM_8, KEY(8), SHIFT_DU(KEY_SHIFT(QUOTE), KEY(8)));
-            CASE_US(CSTM_9, KEY(9), SHIFT_DU(KEY_SHIFT(8), KEY(9)));
-            CASE_US(DEL, KEY(DELETE), KEY_UPSHIFT(BSPACE));
-            CASE_US(TILD, KEY_SHIFT(GRAVE), KEY_SHIFT(EQUAL));
-            CASE_US(EXLM, KEY_SHIFT(1), KEY_SHIFT(1));
-            CASE_US(AT, KEY_SHIFT(2), KEY(LBRACKET));
-            CASE_US(HASH, KEY_SHIFT(3), KEY_SHIFT(3));
-            CASE_US(DLR, KEY_SHIFT(4), KEY_SHIFT(4));
-            CASE_US(PERC, KEY_SHIFT(5), KEY_SHIFT(5));
-            CASE_US(CIRC, KEY_SHIFT(6), KEY(EQUAL));
-            CASE_US(AMPR, KEY_SHIFT(7), KEY_SHIFT(6));
-            CASE_US(ASTR, KEY_SHIFT(8), KEY_SHIFT(QUOTE));
-            CASE_US(LPRN, KEY_SHIFT(9), KEY_SHIFT(8));
-            CASE_US(RPRN, KEY_SHIFT(0), KEY_SHIFT(9));
-            CASE_US(KC_LBRC, KEY(LBRACKET), SHIFT_DU(KEY_SHIFT(RBRACKET), KEY(RBRACKET)));
-            CASE_US(KC_RBRC, KEY(RBRACKET), SHIFT_DU(KEY_SHIFT(NONUS_HASH), KEY(NONUS_HASH)));
-            CASE_US(LCBR, KEY_SHIFT(LBRACKET), KEY_SHIFT(RBRACKET));
-            CASE_US(RCBR, KEY_SHIFT(RBRACKET), KEY_SHIFT(NONUS_HASH));
-            CASE_US(GRV, KEY(GRAVE), SHIFT_DU(KEY_SHIFT(EQUAL), KEY_SHIFT(LBRACKET)));
-            CASE_US(KC_BSLS, KEY(BSLASH), SHIFT_DU(KEY_SHIFT(INT3), KEY(INT3)));
-            //CASE_US(PIPE, KEY_SHIFT(BSLASH), KEY_SHIFT(INT3));
-            CASE_US(KC_MINS, KEY(MINUS), SHIFT_DU(KEY_SHIFT(INT1), KEY(MINUS)));
-            //CASE_US(UNDS, KEY_SHIFT(MINUS), KEY_SHIFT(INT1));
-            CASE_US(KC_EQL, KEY(EQUAL), SHIFT_DU(KEY_SHIFT(SCOLON), KEY_SHIFT(MINUS)));
-            //CASE_US(KC_PLUS, KEY_SHIFT(EQUAL), KEY_SHIFT(SCOLON));
-            CASE_US(KC_SCLN, KEY(SCOLON), SHIFT_DU(KEY_UPSHIFT(QUOTE), KEY(SCOLON)));
-            CASE_US(KC_QUOT, KEY(QUOTE), SHIFT_DU(KEY_SHIFT(2), KEY_SHIFT(7)));
+#endif
         case JP:
             if (record->event.pressed) {
                 LAYOUT_STATUS = JP_LAYOUT;
@@ -172,46 +179,46 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
             break;
-/*        case UBUNTU:
-            if (record->event.pressed) {
-                MACHINE_MODE = UBUNTU_MODE;
-            }
-            return false;
-            break;
-        case MAC:
-            if (record->event.pressed) {
-                MACHINE_MODE = MAC_MODE;
-            }
-            return false;
-            break;
-        case DESKDN:
-            if (record->event.pressed) {
-                if (MACHINE_MODE == UBUNTU_MODE) {
-                    register_code(KC_LGUI);
-                    tap_code(KC_PGDN);
-                    unregister_code(KC_LGUI);
-                } else {
-                    register_code(KC_LCTL);
-                    tap_code(KC_LEFT);
-                    unregister_code(KC_LCTL);
-                }
-            }
-            return false;
-            break;
-        case DESKUP:
-            if (record->event.pressed) {
-                if (MACHINE_MODE == UBUNTU_MODE) {
-                    register_code(KC_LGUI);
-                    tap_code(KC_PGUP);
-                    unregister_code(KC_LGUI);
-                } else {
-                    register_code(KC_LCTL);
-                    tap_code(KC_RIGHT);
-                    unregister_code(KC_LCTL);
-                }
-            }
-            return false;
-            break;*/
+            /*        case UBUNTU:
+                        if (record->event.pressed) {
+                            MACHINE_MODE = UBUNTU_MODE;
+                        }
+                        return false;
+                        break;
+                    case MAC:
+                        if (record->event.pressed) {
+                            MACHINE_MODE = MAC_MODE;
+                        }
+                        return false;
+                        break;
+                    case DESKDN:
+                        if (record->event.pressed) {
+                            if (MACHINE_MODE == UBUNTU_MODE) {
+                                register_code(KC_LGUI);
+                                tap_code(KC_PGDN);
+                                unregister_code(KC_LGUI);
+                            } else {
+                                register_code(KC_LCTL);
+                                tap_code(KC_LEFT);
+                                unregister_code(KC_LCTL);
+                            }
+                        }
+                        return false;
+                        break;
+                    case DESKUP:
+                        if (record->event.pressed) {
+                            if (MACHINE_MODE == UBUNTU_MODE) {
+                                register_code(KC_LGUI);
+                                tap_code(KC_PGUP);
+                                unregister_code(KC_LGUI);
+                            } else {
+                                register_code(KC_LCTL);
+                                tap_code(KC_RIGHT);
+                                unregister_code(KC_LCTL);
+                            }
+                        }
+                        return false;
+                        break;*/
         case SHIFT:
             if (record->event.pressed) {
                 SEND_STRING(SS_DOWN(X_LSHIFT));
@@ -222,36 +229,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
             break;
-        /*case LOWER:
-            if (record->event.pressed) {
-                layer_on(_LOWER);
-            } else {
-                layer_off(_LOWER);
-            }
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            return false;
-            break;
-        case RAISE:
-            if (record->event.pressed) {
-                layer_on(_RAISE);
-            } else {
-                layer_off(_RAISE);
-            }
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            return false;
-            break;
-        case ADJUST:
-            if (record->event.pressed) {
-                layer_on(_ADJUST);
-            } else {
-                layer_off(_ADJUST);
-            }
-            return false;
-            break;
-        default:
-            result = true;
-            break;*/
+            /*case LOWER:
+                if (record->event.pressed) {
+                    layer_on(_LOWER);
+                } else {
+                    layer_off(_LOWER);
+                }
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+                return false;
+                break;
+            case RAISE:
+                if (record->event.pressed) {
+                    layer_on(_RAISE);
+                } else {
+                    layer_off(_RAISE);
+                }
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+                return false;
+                break;
+            case ADJUST:
+                if (record->event.pressed) {
+                    layer_on(_ADJUST);
+                } else {
+                    layer_off(_ADJUST);
+                }
+                return false;
+                break;
+            default:
+                result = true;
+                break;*/
     }
-    
+
     return true;
 }
